6b4ba76fa7aee4a8022bed615c6060afdb2bdce9
kartik goyal <kkartik>
2025-03-12 14:33:36 +0530

    swagger api documentation/configuration

--- productlist/src/main/java/com/kartik/product/dto/CategoryDTO.java
+++ productlist/src/main/java/com/kartik/product/dto/CategoryDTO.java
@@ -1,10 +1,15 @@
 package com.kartik.product.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.List;
 
+@Schema(
+        name="Category",
+        description = "It hold category information"
+)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- productlist/src/main/java/com/kartik/product/dto/ProductDTO.java
+++ productlist/src/main/java/com/kartik/product/dto/ProductDTO.java
@@ -1,9 +1,14 @@
 package com.kartik.product.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(
+        name="Product",
+        description = "It hold product information"
+)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
